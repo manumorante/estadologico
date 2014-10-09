4a78357e61b7eb9834a675c77e77f7174f8c3ccc
Manu Morante <manuel.morante@razorfishhealthware.com>
2014-10-09 12:19:30 +0200

    initial gh-pages commit

---
+++ source/js/application.js
@@ -0,0 +1,9 @@
+//
+// APPLICATION
+//
+
+// Include jQuery
+//= require libs/jquery-1.10.2.min
+
+// Include all compiled Twitter Bootstrap plugins (below), or include individual files as needed
+//= require libs/bootstrap.min
